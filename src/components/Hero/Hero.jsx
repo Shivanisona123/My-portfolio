96f7acf4a736ff0c3862080711e08391b353c644
Shivanisona123 <jaiswalshivani108@gmail.com>
2024-10-06 20:58:43 +0530

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi I'am shivani jaiswal</h1>
+        <p className={styles.description}>
+        Front-end developer specializing in HTML, CSS, JavaScript, and React.js to create responsive, user-friendly interfaces and seamless web experiences.
+        </p>
+        <a href="mailto:jaiswalshivani108@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
